1d0671c0c9036df0d9a997a2125b2d3566535f7f
nkdey14 <im.pandey264@gmail.com>
2023-05-11 22:16:00 +0530

    Blank Project

---
+++ src/main/java/com/cintrix_2/Cintrix2Application.java
@@ -0,0 +1,13 @@
+package com.cintrix_2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Cintrix2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Cintrix2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/cintrix_2/Cintrix2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cintrix_2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Cintrix2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
